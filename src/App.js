9a06126c385629a66127213ce79e62c02f0b561c
fatemahAlMomen <fatemah.alm@hotmail.com>
2023-05-28 12:55:46 +0300

    cookie shop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import "./assets/images";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My awesome shop</h1>
+      <p> my amazing cookie shop</p>
+      <img src={require("./assets/images/shop.jpg")} alt="shop" />
     </div>
   );
 }
